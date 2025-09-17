28a4befb6adb0ed6b118e5796107e27cb319c1dd
Alfred Roos <alfred@stensatter.se>
2025-09-17 15:17:19 +0200

    changed a bit in the dashboard and added a graph

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState} from 'react';
 import type { User } from '~/models/User';
-
+import LineChartComponent from "~/components/LineChartComponent"
 
 const Dashboard = () => {
 
@@ -23,29 +23,51 @@ const Dashboard = () => {
 	}
 
 	return (
-		<main className='w-full h-screen grid grid-col-5 gap-5 px-2 py-5'>
-			<div className='gap-5 grid grid-cols-2 grid-rows-2 w-full row-span-2'>
-				<div className="rounded-xl bg-base-200 flex items-center justify-center">
-					<p  className='text-5xl'>45</p>
+		<main className='container mx-auto w-full h-screen gap-5 px-2 py-5'>
+			<div className='gap-5 grid grid-cols-2 grid-rows-2 w-full row-span-2 h-2/5'>
+				<div className="stats shadow bg-base-300">
+					<div className="stat">
+						<div className="stat-title">Total Page Views</div>
+						<div className="stat-value">89,400</div>
+						<div className="stat-desc">21% more than</div>
+					</div>
 				</div>
-				<div className="rounded-xl bg-base-300 row-span-2 flex items-center justify-center">
-					<p  className='text-5xl'>45</p>
+				<div className="stats shadow bg-base-200 row-span-2">
+					<div className="stat">
+						<div className="stat-title">Total Page Views</div>
+						<div className="stat-value">89,400</div>
+						<div className="stat-desc">21% more </div>
+					</div>
 				</div>
-				<div className="rounded-xl bg-secondary flex items-center justify-center">
-					<p  className='text-5xl'>45</p>
+				<div className="stats shadow bg-base-200">
+					<div className="stat">
+						<div className="stat-title">Total Page Views</div>
+						<div className="stat-value">89,400</div>
+						<div className="stat-desc">21% more than</div>
+					</div>
 				</div>
 			</div>
-			<div className="row-span-2">
-				<h2 className='text-3xl font-bold'>Weekly Stregth</h2>
+			<div className="row-span-2 mt-7">
+				<h2 className='text-2xl font-semibold'>Weekly Stregth</h2>
 			</div>
-			<div className="flex flex-row items-end gap-2 w-full justify-around">
-				<button className='btn btn-lg btn-secondary'>
-					Log Excercise
-				</button>
-				<button className='btn btn-lg btn-secondary'>
-					Log Weight
-				</button>
+			<div className='flex flex-col justify-between h-3/7 mt-5'>
+				<div className="bg-base-200 flex h-full w-full rounded-lg">
+					<LineChartComponent>
+					</LineChartComponent>
+				</div>
+
+				<div className="mt-5 flex flex-row items-end gap-2 w-full justify-around">
+					<button className='btn btn-lg btn-secondary'>
+						Log Excercise
+					</button>
+					<button className='btn btn-lg btn-secondary'>
+						Log Weight
+					</button>
+				</div>
 			</div>
+
+
+
 		</main>
 	)
 }
