8495fc8dd1871f5c73ea71542cc8a2d4364f5d38
AnonyTQ <leo.modin@hotmail.se>
2025-09-17 13:09:17 +0200

    added regex once again...

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -4,7 +4,16 @@ import { Router, Request, Response, NextFunction } from "express";
 
 export function userCreate(req: Request, res: Response)
 {
-    User.create([
+
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if(!emailRegex.test(req.body.email))
+  {
+    return res
+      .status(400)
+      .json(new ApiResponse({status: "fail", data: "Invalid email format"}));
+  }
+  User.create([
     { name: req.body.userName, email: req.body.email, password: req.body.password},
   ]).then(done=>{
     res.json(new ApiResponse({data:"added"}))
