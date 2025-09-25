846e7e262a2ecf18ce3754f43c6c3edae997bb27
Alfred <alfred@stensatter.se>
2025-09-25 23:06:01 +0200

    fixed route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -6,4 +6,5 @@ export default [
   route("dashboard", "routes/dashboard.tsx"), // About at /about
   route("profile", "routes/profile.tsx"),
   route("login", "routes/login/login.tsx"),
+  route("leaderboard", "routes/leaderboard.tsx"),
 ] satisfies RouteConfig;
