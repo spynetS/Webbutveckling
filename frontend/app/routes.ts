1724ac82af818bf8a2fc015752a7fa105d1d975a
AnonyTQ <mole23jk@student.ju.se>
2025-09-25 10:43:16 +0200

    leaderboard frontend template

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"),
                 route("signup", "routes/signup.tsx"),
-				route("dashboard", "routes/dashboard.tsx") // About at /about
-			   ] satisfies RouteConfig;
+				route("dashboard", "routes/dashboard.tsx"), // About at /about
+			   	route("leaderboard", "routes/leaderboard.tsx")
+			] satisfies RouteConfig;
